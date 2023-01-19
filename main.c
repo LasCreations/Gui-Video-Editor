d3bd373a3b1484f9b01957fce0c369c4890f69cb
LasCreations <lascellemckenzie@gmail.com>
2023-01-19 17:51:43 -0500

    Edit ReadMe

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
+//Standard Library
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//Gui Library
 #include <gtk/gtk.h>
 
+//
 #include <libavcodec/avcodec.h>
 
 #include <libavutil/opt.h>
 #include <libavutil/imgutils.h>
 
-
 #include <libavformat/avformat.h>
+
 #include <libswresample/swresample.h>
 
 
@@ -20,7 +23,6 @@ static void activate(GtkApplication* app, gpointer user_data){
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
   gtk_widget_show_all (window);
-
 }
 
 int main(int argc,char **argv){
